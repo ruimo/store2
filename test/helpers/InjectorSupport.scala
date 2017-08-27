de4c09918bae5bd30c5c771192ec06de267ce863
Shisei Hanai <ruimo.uno@gmail.com>
2017-08-27 11:07:00 +0900

    Set gecko driver path.

--- test/helpers/InjectorSupport.scala
+++ test/helpers/InjectorSupport.scala
@@ -6,6 +6,8 @@ import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.test.Helpers._
 
 trait InjectorSupport {
+  System.setProperty("webdriver.gecko.driver", System.getenv("GECKO_DRIVER_PATH"))
+
   def appl(conf: Map[String, Any] = inMemoryDatabase()): Application = GuiceApplicationBuilder().configure(conf).build()
 
   def inject[T](implicit app: Application, c: ClassTag[T]): T = app.injector.instanceOf[T]
