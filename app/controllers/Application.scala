fa81a6794d4695cd341ed7a33f8aa149ba80702f
Shisei Hanai <ruimo.uno@gmail.com>
2017-08-30 22:18:06 +0900

    Show user metadata.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,5 +1,8 @@
 package controllers
 
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+import models._
 import play.api.mvc._
 import javax.inject.{Inject, Singleton}
 
@@ -11,14 +14,24 @@ class Application @Inject() (
   cc: MessagesControllerComponents,
   optAuthenticated: NeedLogin.OptAuthenticated,
   loginSessionRepo: LoginSessionRepo,
+  val storeUserRepo: StoreUserRepo,
   implicit val db: Database,
   implicit val shoppingCartItemRepo: ShoppingCartItemRepo
 ) extends MessagesAbstractController(cc) {
   def index = optAuthenticated { implicit request: MessagesRequest[AnyContent] =>
-    implicit val optLogin = db.withConnection { implicit conn =>
-      loginSessionRepo.fromRequest(request)
+    db.withConnection { implicit conn =>
+      implicit val optLogin = loginSessionRepo.fromRequest(request)
+      Ok(
+        views.html.index(
+          UserMetadata.recentlyJoindUsers(Instant.now().plus(-30, ChronoUnit.DAYS)).map { um =>
+            (storeUserRepo(um.storeUserId), um)
+          },
+          UserMetadata.nearBirthDayUsers().map { um =>
+            (storeUserRepo(um.storeUserId), um)
+          }
+        )
+      )
     }
-    Ok(views.html.index())
   }
 
   def notFound(path: String) = Action {
