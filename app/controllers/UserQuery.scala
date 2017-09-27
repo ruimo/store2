5b57e115d1026826055adffe38f20fd6f11a8ee6
花井志生 <shanai@hanaishisei-no-MacBook-Pro-2.local>
2017-09-27 21:01:09 +0900

    User list

---
+++ app/controllers/UserQuery.scala
@@ -0,0 +1,43 @@
+package controllers
+
+import helpers.Cache
+import play.api.i18n.Lang
+import models._
+
+import collection.immutable
+import play.api.data.Form
+import play.api.data.Forms._
+import java.sql.Connection
+import javax.inject.{Inject, Singleton}
+
+import controllers.NeedLogin.Authenticated
+import play.api.db.Database
+import play.api.mvc._
+
+@Singleton
+class UserQuery @Inject() (
+  cc: MessagesControllerComponents,
+  authenticated: Authenticated,
+  cache: Cache,
+  val storeUserRepo: StoreUserRepo,
+  implicit val db: Database,
+  implicit val shoppingCartItemRepo: ShoppingCartItemRepo
+) extends MessagesAbstractController(cc) {
+  def usersCanViewProfiles: Boolean = cache.config(_.get[Boolean]("usersCanViewProfiles"))()
+
+  def index(
+    page: Int, pageSize: Int, orderBySpec: String
+  ) = authenticated { implicit request: AuthMessagesRequest[AnyContent] =>
+    implicit val login = request.login
+    if (! usersCanViewProfiles) Redirect(routes.Application.index)
+    else {
+      db.withConnection { implicit conn =>
+        Ok(
+          views.html.listUsers(
+            storeUserRepo.listUsers(page, pageSize, OrderBy(orderBySpec))
+          )
+        )
+      }
+    }
+  }
+}
